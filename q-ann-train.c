1b8a1cbf03a97751f08de44672d0ec06ffb6c289
rafaabt <rafael.billig.tonetto@gmail.com>
2023-11-11 14:12:37 -0300

    .

--- q-ann-train.c
+++ q-ann-train.c
@@ -22,31 +22,30 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 int main(int argc, char **argv)
 {
+    const unsigned int num_input  = 2;  // state and action
+    const unsigned int num_output = 1;  // state-action 'Q' value
+    const unsigned int num_layers = 3;
+    const unsigned int num_neurons_hidden = 8;
+    const float desired_error = (const float) 0.001;
+    const unsigned int max_epochs = 50000;
+    const unsigned int epochs_between_reports = 1000;
 
-	const unsigned int num_input  = 2;  // state and action
-	const unsigned int num_output = 1;  // state-action 'Q' value
-	const unsigned int num_layers = 3;
-	const unsigned int num_neurons_hidden = 8;
-	const float desired_error = (const float) 0.001;
-	const unsigned int max_epochs = 50000;
-	const unsigned int epochs_between_reports = 1000;
+    struct fann *ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
 
-	struct fann *ann = fann_create_standard(num_layers, num_input, num_neurons_hidden, num_output);
+    fann_set_activation_function_hidden(ann, FANN_SIGMOID);//_SYMMETRIC);
+    fann_set_activation_function_output(ann, FANN_SIGMOID);//_SYMMETRIC);
 
-	fann_set_activation_function_hidden(ann, FANN_SIGMOID);//_SYMMETRIC);
-	fann_set_activation_function_output(ann, FANN_SIGMOID);//_SYMMETRIC);
+    /*
+        fann_set_training_algorithm()
+        The default training algorithm is FANN_TRAIN_RPROP. (fann_train.h)
+    */
 
-	/*
-		fann_set_training_algorithm()
+    //fann_set_training_algorithm(ann, FANN_TRAIN_BATCH); // ver algs em fann_data.h
 
-		   The default training algorithm is FANN_TRAIN_RPROP. (fann_train.h)
-	*/
-	//fann_set_training_algorithm(ann, FANN_TRAIN_BATCH); // ver algs em fann_data.h
+    fann_train_on_file(ann, "q-learn-data.txt", max_epochs, epochs_between_reports, desired_error);
+    fann_save(ann, "q-learn.net");
+    fann_print_connections(ann);
+    fann_destroy(ann);
 
-	fann_train_on_file(ann, "q-learn-data.txt", max_epochs, epochs_between_reports, desired_error);
-	fann_save(ann, "q-learn.net");
-	fann_print_connections(ann);
-	fann_destroy(ann);
-
-	return 0;
+    return 0;
 }
